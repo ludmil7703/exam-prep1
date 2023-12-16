2dfa9190c30bd1bb816a7de665dd812edef35c30
ludmil7703 <106627152+ludmil7703@users.noreply.github.com>
2023-12-16 12:49:35 +0200

    Change the URL of tests.

--- tests/ui/books.test.js
+++ tests/ui/books.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check books page', async ({ page }) => {
-    await page.goto('http://localhost:8080/books');
+    await page.goto('https://ludmil7703-exam-prep1.onrender.com/books');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
